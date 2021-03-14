491f0512951a2582e5b4516df521105f45e21324
dennis <travlInfo4you@gmail.com>
2021-03-14 16:01:11 +0900

    로그인주소 완성

--- app/src/main/java/kr/co/tjoeun/serverapp_daily10minutes_20210314/utils/ServerUtil.kt
+++ app/src/main/java/kr/co/tjoeun/serverapp_daily10minutes_20210314/utils/ServerUtil.kt
@@ -3,18 +3,21 @@ package kr.co.tjoeun.serverapp_daily10minutes_20210314.utils
 class ServerUtil {
 //    API를 호출해주는 함수들을 모아두기 위한 크래스.(코드 정리 차원)
 
-//    호스트 주소를 편하게 입력/관리하기 위한 변수
-    val HOST_URL = "http://15.164.153.174"
+
 
 //    ServerUtil.함수() 처럼, 클래이름. 만 해도 바로 사용하게 도와주는 코드
 //    JAVA - static 개념에 대으되는 코드.
 
     companion object{
+        //    호스트 주소를 편하게 입력/관리하기 위한 변수
+        val HOST_URL = "http://15.164.153.174"
 
 //      함수 작성 - 로그인 기능 담당 함수.
 
         fun postRequestLogin() {
+//            실제 기능 수행주소 ex. 로그인 - http://15.164.153.174/user
 
+            val urlString = "${HOST_URL}/user"
 
         }
     }
