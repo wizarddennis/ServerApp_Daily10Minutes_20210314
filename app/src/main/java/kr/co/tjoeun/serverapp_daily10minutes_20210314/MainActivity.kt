127417676880dd151587795fafc94958d1dbb0f5
dennis <travlInfo4you@gmail.com>
2021-03-14 15:42:49 +0900

    OkHTTP 라이브러리 설치 + ServerUtil 클래스 추가

--- app/src/main/java/kr/co/tjoeun/serverapp_daily10minutes_20210314/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/serverapp_daily10minutes_20210314/MainActivity.kt
@@ -19,6 +19,7 @@ class MainActivity : BaseActivity() {
             val inputPw = pwEdt.text.toString()
             
             // 이 아이디/비번이 회원이 맞는지, 서버에 확인 요청. => 로그인 요청
+
         }
 
     }
---
+++ app/src/main/java/kr/co/tjoeun/serverapp_daily10minutes_20210314/utils/ServerUtil.kt
@@ -0,0 +1,5 @@
+package kr.co.tjoeun.serverapp_daily10minutes_20210314.utils
+
+class ServerUtil {
+//    API를 호출해주는 함수들을 모아두기 위한 크래스.(코드 정리 차원)
+}
