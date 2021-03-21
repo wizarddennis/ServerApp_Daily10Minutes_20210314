a8065d31cd5e24f63b9ee3c7fa344f0fccbd9732
dennis <travlInfo4you@gmail.com>
2021-03-21 16:14:40 +0900

    실제로 토큰값 저장

--- app/src/main/java/kr/co/tjoeun/serverapp_daily10minutes_20210314/LoginActivity.kt
+++ app/src/main/java/kr/co/tjoeun/serverapp_daily10minutes_20210314/LoginActivity.kt
@@ -63,6 +63,8 @@ class LoginActivity : BaseActivity() {
                         val dataObj = json.getJSONObject("data")
                         val token = dataObj.getString("token")
 
+                        ContextUtil.setToken(mContext, token)
+
 
 
                         val myIntent = Intent(mContext, MainActivity::class.java)
