848ad9d689ff0882c04f0ff79300a0426504b7b2
dennis <travlInfo4you@gmail.com>
2021-03-21 13:54:36 +0900

    프로젝트목록 추가작업 - 서버에서 받아오기전

--- app/src/main/java/kr/co/tjoeun/serverapp_daily10minutes_20210314/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/serverapp_daily10minutes_20210314/MainActivity.kt
@@ -2,18 +2,30 @@ package kr.co.tjoeun.serverapp_daily10minutes_20210314
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.tjoeun.serverapp_daily10minutes_20210314.datas.Project
 
 class MainActivity : BaseActivity() {
-    override fun setupEvents() {
-        TODO("Not yet implemented")
-    }
 
-    override fun setValues() {
-        TODO("Not yet implemented")
-    }
+    val mProjectList = ArrayList <Project> ()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupEvents()
+        setValues()
     }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+//        메인화면에 들어오면 => 프로젝트 목록이 뭐뭐있는지 서버에 요청(ServerUtil 함수 추가)
+//        받아온 결과를 분석해서=> Proejct() 형태로 만들어서 => mProjectList에 add 해주자.
+
+
+    }
+
+
 }
