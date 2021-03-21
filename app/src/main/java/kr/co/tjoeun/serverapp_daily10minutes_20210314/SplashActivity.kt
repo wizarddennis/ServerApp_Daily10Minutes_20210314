af1ecf4fce059cb9e816772fdf9f8c90d54ed192
dennis <travlInfo4you@gmail.com>
2021-03-21 16:47:36 +0900

    토큰이 있고, 자동로그인 OK라면 => 바로 메인으로 가도록(자동로그인 구현)

--- app/src/main/java/kr/co/tjoeun/serverapp_daily10minutes_20210314/SplashActivity.kt
+++ app/src/main/java/kr/co/tjoeun/serverapp_daily10minutes_20210314/SplashActivity.kt
@@ -2,6 +2,9 @@ package kr.co.tjoeun.serverapp_daily10minutes_20210314
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import kr.co.tjoeun.serverapp_daily10minutes_20210314.utils.ContextUtil
 
 class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +20,33 @@ class SplashActivity : BaseActivity() {
     }
 
     override fun setValues() {
+//        앱 실행후 3추가 지나면 => 무조건 메인으로.(예전)
+
+//        앱 실행후 3초가 지나면 => 토큰있고, 자동로그인OK => 메인화면..하나라도 아니면 로그인화면..
+        val myHandler = Handler(Looper.getMainLooper())
+
+        myHandler.postDelayed({
+
+//            저장된 토큰/자동로그인 여부 확인.
+            val savedToken = ContextUtil.getToken(mContext)
+            val isAutoLogin = ContextUtil.getAutoLogin(mContext)
+
+//            토큰이 있는가? => 커낸 토큰이 빈칸이 아닌가?
+//            자동로그인 하는가? => 꺼낸 Boolean값이 true?
+            if( savedToken != "" && isAutoLogin ) {
+//                바로 메인화면으로 이동.
+                val myIntent = Intent(mContext, MainActivity::class.java)
+                startActivity(myIntent)
+            }
+            else {
+//                로그인화면으로 이동.
+                val myIntent = Intent(mContext, LoginActivity::class.java)
+                startActivity(myIntent)
+            }
+
+            finish()
+
+        }, 3000)
 
     }
 
