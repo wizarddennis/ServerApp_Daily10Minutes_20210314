188d05eebcce162358cc5e4955db303ad13504f3
dennis <travlInfo4you@gmail.com>
2021-03-21 10:47:39 +0900

    회원가입 API 호출까지

--- app/src/main/java/kr/co/tjoeun/serverapp_daily10minutes_20210314/SignUpActivity.kt
+++ app/src/main/java/kr/co/tjoeun/serverapp_daily10minutes_20210314/SignUpActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.provider.ContactsContract
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_sign_up.*
+import kr.co.tjoeun.serverapp_daily10minutes_20210314.utils.ServerUtil
+import org.json.JSONObject
 
 class SignUpActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +24,14 @@ class SignUpActivity : BaseActivity() {
             val pw = pwEdt.text.toString()
             val nick = nicknameEdit.text.toString()
 
-//            서버 - 회원가입 기능에 : ServerUtil 에
+//            서버 - 회원가입 기능에 전송 : ServerUtil 에 회원가입 함수 필요
+            ServerUtil.putRequestSignUp(email, pw, nick, object : ServerUtil.JsonResponseHandler {
+                override fun onResponse(json: JSONObject) {
+
+                }
+
+            })
+
         }
 
     }
